d8acaf1984a0fbdce02a1e71a85f84c886c0e151
EastShine <75498839+eastshine12@users.noreply.github.com>
2026-06-01 14:08:42 +0900

    chore: 백엔드 Gradle 프로젝트 스캐폴딩 추가

---
+++ src/main/kotlin/com/replus/api/ReplUsBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.replus.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReplUsBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReplUsBackendApplication>(*args)
+}
